6207de2217d85810fd1c23296d526862bb7e0b4d
Takuya Ohsawa <da87435@gmail.com>
2025-01-28 18:18:26 +0900

    Testを更新

--- CounterTests/CounterIntentTests.swift
+++ CounterTests/CounterIntentTests.swift
@@ -1,11 +1,35 @@
 import Testing
 @testable import Counter
 
+// MARK: - API Service
+final class TestCounterAPIService: APIService {
+    private let fetchCount: Int
+    
+    init(fetchCount: Int = 42) {
+        self.fetchCount = fetchCount
+    }
+    
+    func fetchCount() async throws -> Int {
+        return fetchCount // サーバーから取得した仮のデータ
+    }
+
+    func updateCount(newCount: Int) async throws {}
+}
+
 struct CounterIntentTests {
+    
+    @Test func onAppearの時に最新の値で更新される() async throws {
+        let viewState = CounterViewState()
+        let apiService = TestCounterAPIService(fetchCount: 100)
+        let intent = CounterIntent(viewState: viewState, apiService: apiService)
+        intent.handleOnAppear()
+        
+        #expect(viewState.count == 100)
+    }
 
     @Test func Incrementをタップすると値が増加する() async throws {
         let viewState = CounterViewState()
-        let apiService = CounterAPIService()
+        let apiService = TestCounterAPIService()
         let intent = CounterIntent(viewState: viewState, apiService: apiService)
         intent.tapIncrement()
         
@@ -14,7 +38,7 @@ struct CounterIntentTests {
     
     @Test func Decrementをタップすると値が増加する() async throws {
         let viewState = CounterViewState()
-        let apiService = CounterAPIService()
+        let apiService = TestCounterAPIService()
         let intent = CounterIntent(viewState: viewState, apiService: apiService)
         intent.tapDecrement()
         
@@ -23,7 +47,7 @@ struct CounterIntentTests {
     
     @Test func リセットをタップすると値がリセットされる() async throws {
         let viewState = CounterViewState()
-        let apiService = CounterAPIService()
+        let apiService = TestCounterAPIService()
         let intent = CounterIntent(viewState: viewState, apiService: apiService)
         intent.tapDecrement()
         
